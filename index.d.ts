85fd1a62f95ace09330ffabbb2bfea70e3fd6622
dosomder <dosomder@users.noreply.github.com>
2020-05-29 22:14:55 +0200

    Add ts declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,25 @@
-//--------------------------
-// keyboardshowing typings file.
-//--------------------------
+declare module "nativescript-keyboardshowing" {
+  /**
+   * Returns if the keyboard is showing.
+   */
+  export function isShowing(): boolean;
+
+  /**
+   * Refresh the keyboard listeners in case tracking is lost.
+   */
+  export function refreshListener(): void;
+
+  /**
+   * Add a new observer for the keyboard state.
+   * @param callback Callback to call when the state of the keyboard changes
+   */
+  export function addNotifyKeyboardAction(callback: (state: any) => void): void;
+
+  /**
+   * Remove a previously registered observer.
+   * @param callback Callback to remove
+   */
+  export function removeNotifyKeyboardAction(
+    callback: (state: any) => void
+  ): void;
+}
